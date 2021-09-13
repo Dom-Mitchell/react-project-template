c1387a354c77bd190badcff039bf3997c8c03b30
Domenick Mitchell <domenickmitchell00@gmail.com>
2021-09-12 20:48:10 -0400

    Added .keep file to component folder

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export function App() {
   return (
     <div>
-      <p>Hello, & Dom!</p>
+      <p>Hello, Dom!</p>
       <footer>
         <img src="icons/footer/favicon.png" alt="Monk" />
       </footer>
