dcd9e4fdee571028d7a57441182118124f2a5ca4
Domenick Mitchell <domenickmitchell00@gmail.com>
2021-09-13 21:12:42 -0400

    Image Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import monk from './icons/footer/favicon.png'
 
 export function App() {
   return (
     <div>
       <p>Hello, Dom!</p>
       <footer>
-        <img src="icons/footer/favicon.png" alt="Monk" />
+        <img src={monk} alt="Monk" />
       </footer>
     </div>
   )
